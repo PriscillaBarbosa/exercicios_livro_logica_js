ab2819241812bcc77814926865789ead80adebeb
Priscilla Barbosa <priscillabarbosa@outlook.com>
2024-07-06 12:24:31 -0300

    alteração na atividade 2.10b

--- cap02/ex2.10/b/lanHouse.js
+++ cap02/ex2.10/b/lanHouse.js
@@ -1,15 +1,16 @@
-//criar referência ao formulário e ao elemento h3
+//criar referência ao formulário e ao elemento h3(onde será exibido a resposta)
 const frm = document.querySelector("form")
-const pagar = document.getElementById("valorApagar")
+const resp = document.querySelector("h3")
 
 //criar um ouvinte de evento acionado quando o botão submit for criado
 frm.addEventListener("submit", (e) =>{
+    //obtem conteúdo e converte em número
     const valor = Number(frm.inValor.value) //obtem o valor do tempo usado
-    const tempo = Time(frm.inTempo.value) //obtem o tempo utilizado
+    const tempo = Number(frm.inTempo.value) //obtem o tempo utilizado
 
-    const valorFinal = tempo * valor //obtem o valor a ser pago
+    const pagar = Math.ceil(tempo / 15) * valor //obtem o valor a ser pago
 
-    pagar.innerText = `Valor a pagar R$: ${valorFinal.toFixed(2)}`   //exibir o valor a pagar
+    resp.innerText = `Valor a pagar R$: ${pagar.toFixed(2)}`   //exibir o valor a pagar com 2 decimais
 
     e.preventDefault()
 })
