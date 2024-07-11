b729b497c0e6cf40d8b0cc1bc5054302a9bfdded
Priscilla Barbosa <priscillabarbosa@outlook.com>
2024-07-10 22:02:01 -0300

    cap04 ex01

--- cap02/ex2.4/index.js
+++ cap02/ex2.4/index.js
@@ -4,7 +4,7 @@ const resp = document.querySelector("h3")
 
 //criar um "ouvinte" de evento, acionado quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-    const quilo = Number(frm.inquilo.value)  //para obter o conteúdo dos campos
+    const quilo = Number(frm.inQuilo.value)  //para obter o conteúdo dos campos
     const consumo = Number(frm.inConsumo.value)
 
     const valor = (quilo / 1000) * consumo //para calcular o valor a ser pago
