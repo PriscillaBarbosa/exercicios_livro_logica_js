aae0806cc8eca7ac13fe0b9649c2a998592319a4
PRISCILLA B <priscillabarbosa@outlook.com>
2025-07-08 02:23:31 +0000

    adicionando parte 1 exercício

---
+++ cap06/js/ex6_3.js
@@ -0,0 +1,7 @@
+const frm = document.querySelector("form") //obtém elementos da página
+const resp = document.querySelector("pre")
+const carros = [] //declara vetor global
+
+frm.addEventListener("submit", (e) => { //escuta o evento submit do form
+
+})
